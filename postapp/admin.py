d82084b04bfbc191761d3a5595861323cf9b1fb7
sarthakverma12 <sarthakverma.12@gmail.com>
2021-05-31 11:42:53 +0530

    Fixes

--- postapp/admin.py
+++ postapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post, PostUser, Profile
+from .models import Post, PostUser
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(PostUser)
-admin.site.register(Profile)
